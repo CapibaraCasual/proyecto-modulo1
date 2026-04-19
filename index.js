dbfdf5cb7fc0940656399a58b9aa1d57f5ca82c3
AvispaCulona <Yadirabraham@gmail.com>
2026-04-19 15:44:55 -0500

    descripción del cambio

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hola mundo")
+let gastos = [50, 120, 30, 200, 80]
+
+for (let i = 0; i < gastos.length; i++) {
+    if (gastos[i] > 100) {
+        console.log("Gasto alto: " + gastos[i])
+    } else {
+        console.log("Gasto normal: " + gastos[i])
+    }
+}
